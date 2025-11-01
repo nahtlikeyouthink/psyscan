2cab498ade2b790f2ace84ad2e64c6dd30558339
Naht Like You Think <whoareyounaht@gmail.com>
2025-11-01 21:44:56 +0100

    Update psyscan.py

--- psyscan.py
+++ psyscan.py
@@ -184,8 +184,8 @@ def psi_scan(self, texte: str) -> dict:
   freq = Counter(filtered)
   total = sum(freq.values())  
 if total == 0:
-  return {'error':
-          'Aucun mot significatif détecté'}
+return {'error':
+        'Aucun mot significatif détecté'}
   top = freq.most_common(5)
   s1, count_s1 = top[0]
   centralite = round((count_s1 / total) * 100, 1)
