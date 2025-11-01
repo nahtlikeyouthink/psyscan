436143fd79253e7a8ceb6d6cf3a812c8a61e078b
Naht Like You Think <whoareyounaht@gmail.com>
2025-11-01 20:49:09 +0100

    Update psyscan.py

--- psyscan.py
+++ psyscan.py
@@ -51,11 +51,11 @@ ORAL_ARTIFACTS = {'euh', 'heu', 'hum', 'ah', 'bon', 'voilà', 'donc', 'alors', '
 # ========================
 lemmatizer = WordNetLemmatizer()
 def get_wordnet_pos(treebank_tag):
-if treebank_tag.startswith('V'): return wordnet.VERB
-elif treebank_tag.startswith('N'): return wordnet.NOUN
-elif treebank_tag.startswith('J'): return wordnet.ADJ
-elif treebank_tag.startswith('R'): return wordnet.ADV
-else: return wordnet.NOUN
+    if treebank_tag.startswith('V'): return wordnet.VERB
+    elif treebank_tag.startswith('N'): return wordnet.NOUN
+    elif treebank_tag.startswith('J'): return wordnet.ADJ
+    elif treebank_tag.startswith('R'): return wordnet.ADV
+    else: return wordnet.NOUN
             
 # ========================
 # NER
