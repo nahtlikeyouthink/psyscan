d970af1e4890dbc67474f1b4dac97bd3cdb6f010
Naht Like You Think <whoareyounaht@gmail.com>
2025-11-02 01:31:50 +0100

    Update psyscan.py

--- psyscan.py
+++ psyscan.py
@@ -17,6 +17,8 @@ nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 from nltk.corpus import stopwords, wordnet
 from nltk.stem import WordNetLemmatizer
